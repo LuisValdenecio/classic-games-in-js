8967af9d7cea133d32067234b94feb6cd966aade
Luís Tchitue <ls04af@gmail.com>
2021-12-11 06:13:44 -0300

    food object now responds to its own positioning and not css anymore

--- COBRA-GAME/gameAssets/food.js
+++ COBRA-GAME/gameAssets/food.js
@@ -1,16 +1,24 @@
 class Food {
     
-    static NAME = 'food';
-
     constructor() {
         this.food;
+        this.posX;
+        this.posY;
     }
   
     maker = () => {
+        this.randomPositioner();
         this.food = document.createElement('div');
         this.food.setAttribute('id', 'food');
+        this.food.style.left = `${this.posX}px`;
+        this.food.style.top = `${this.posY}px`;
         return this.food;
     }   
+
+    randomPositioner = () => {
+        this.posX = Math.random() * 100;
+        this.posY = Math.random() * 100;
+    }
 
     broadCastPosition = () => {
         return { x : 547.942, y : this.getHTMLele().style.top }
