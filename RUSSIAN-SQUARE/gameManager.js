3bfebcf33e5bd82e8bce4ba1d82dc03352dd7828
Luís Tchitue <ls04af@gmail.com>
2021-12-19 17:51:20 -0300

    added the mechanics of movement to the game

--- RUSSIAN-SQUARE/gameManager.js
+++ RUSSIAN-SQUARE/gameManager.js
@@ -1,7 +1,29 @@
 class GameManager {
 
-    runGame = () => {
-        window.requestAnimationFrame(this.runGame)
+    constructor() {
+        this.start = null;
+        this.previousTimeStamp = null;
+    }
+
+    runGame = (timestamp) => {
+
+        if (this.start === null) {
+            this.start = timestamp;
+        }
+
+        const elapsed = timestamp - this.start;
+
+        if (this.previousTimeStamp !== timestamp) {
+            // run here some moving game logic
+            
+        }
+
+        if (elapsed <= Infinity) {
+            this.previousTimeStamp = timestamp;
+            window.requestAnimationFrame(this.runGame);
+        }
+
+       
     }
 
 }
